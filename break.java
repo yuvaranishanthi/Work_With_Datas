f2a030ad258d49d3b9b92b78af2723a6c6a0c70a
yuvaranishanthi <146846664+yuvaranishanthi@users.noreply.github.com>
2025-02-27 12:02:20 +0530

    Add files via upload

---
+++ break.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class break
+{
+  public static void main(String[] args)
+  {
+    Scanner shanthi=new Scanner(System.in);
+    int num=shanthi.nextInt(); 
+    for(int i=1; i<=num; i++)
+    {
+        if(i==9)
+        {
+            break;
+        }
+        System.out.println(i +"");
+    }
+
+    
+    }
+   }
